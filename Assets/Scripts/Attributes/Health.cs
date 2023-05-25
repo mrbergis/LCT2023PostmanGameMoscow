591b727c79a5de96eaca608d2635cb1c870e1282
mrbergis <mrbergis@mail.ru>
2023-05-26 00:38:35 +0300

    Вторая сцена 3д

--- Assets/Scripts/Attributes/Health.cs
+++ Assets/Scripts/Attributes/Health.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Attributes
 {
     public class Health : MonoBehaviour
     {
         bool _isDead = false;
+        
+        [field: SerializeField]
+        public UnityEvent OnPlayerDie { get; set; }
         
         public bool IsDead()
         {
@@ -17,6 +21,8 @@ namespace Attributes
 
             _isDead = true;
             GetComponent<Animator>().SetTrigger("Lose");
+            
+            OnPlayerDie?.Invoke();
         }
         
     }
